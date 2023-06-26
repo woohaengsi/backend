bc6df084ec9821e07327fe1d7a295c9deebf11f1
PhilSoGooood <peelhw@gmail.com>
2023-06-26 21:01:58 +0900

    :tada: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/woohaengsi/qnadiary/QnaDiaryApplication.java
@@ -0,0 +1,13 @@
+package woohaengsi.qnadiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woohaengsi/qnadiary/QnaDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package woohaengsi.qnadiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f88086559029a10951e3b79cbc7bbddb4061be26
PhilSoGooood <peelhw@gmail.com>
2023-06-30 20:05:09 +0900

    :wrench: 설정 파일 분리

--- src/main/java/woohaengsi/qnadiary/QnaDiaryApplication.java
+++ src/main/java/woohaengsi/qnadiary/QnaDiaryApplication.java
@@ -2,9 +2,8 @@ package woohaengsi.qnadiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+
 @SpringBootApplication
 public class QnaDiaryApplication {
 

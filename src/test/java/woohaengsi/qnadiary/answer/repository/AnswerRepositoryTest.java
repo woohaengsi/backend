94bc0a13639c37c095534867c40dfa9a8952bb72
PhilSoGooood <peelhw@gmail.com>
2023-07-01 13:14:30 +0900

    :white_check_mark: 특정 연도, 월의 작성일 리스트 조회 기능 테스트
    - 작성일을 수정할 수 있도록 BaseEntity 기능 추가

--- src/test/java/woohaengsi/qnadiary/answer/repository/AnswerRepositoryTest.java
+++ src/test/java/woohaengsi/qnadiary/answer/repository/AnswerRepositoryTest.java
@@ -1,21 +1,28 @@
 package woohaengsi.qnadiary.answer.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static woohaengsi.qnadiary.auth.oauth.type.ResourceServer.APPLE;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import woohaengsi.qnadiary.DatabaseCleanup;
 import woohaengsi.qnadiary.answer.domain.Answer;
-import woohaengsi.qnadiary.auth.oauth.type.ResourceServer;
 import woohaengsi.qnadiary.member.domain.Member;
 import woohaengsi.qnadiary.member.repository.MemberRepository;
 import woohaengsi.qnadiary.question.domain.Question;
 import woohaengsi.qnadiary.question.repository.QuestionRepository;
 
-@DataJpaTest
+@ActiveProfiles("test")
+@SpringBootTest
 class AnswerRepositoryTest {
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
     @Autowired
     private AnswerRepository answerRepository;
     @Autowired
@@ -23,12 +30,27 @@ class AnswerRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @AfterEach
+    void tearDown() {
+        databaseCleanup.afterPropertiesSet();
+        databaseCleanup.execute();
+    }
+
+    @BeforeEach
+    void setUp() {
+        questionRepository.save(new Question("1번 질문"));
+        questionRepository.save(new Question("2번 질문"));
+        memberRepository.save(Member.of(APPLE, "resource_id", "name", "email", "image"));
+    }
+
+
     @Test
     @DisplayName("답변을 생성하면 저장된 답변의 갯수가 증가한다")
     void create_increase_size() {
     	// given
-        Member member = Member.of(ResourceServer.KAKAO, "TEST", "TEST", "TEST", "TEST");
-        Question question = new Question("질문1");
+        Member member = memberRepository.findById(1L).orElseThrow(IllegalArgumentException::new);
+        Question question = questionRepository.findById(1L)
+            .orElseThrow(IllegalArgumentException::new);
         String content = "답변1";
         Answer answer = new Answer(member, question, content);
         int oldSize = answerRepository.findAll().size();

3f48cfc856253c22a8c1113aceafb525fbe9ea37
PhilSoGooood <peelhw@gmail.com>
2023-08-01 19:17:36 +0900

    :white_check_mark: Answer 레포지토리 통합 테스트
    - @DataJpaTest는 기본적으로 임베디드 인메모리 데이터베이스를 사용하기 떄문에 @AutoConfigureTestDatabase를 수정해주었습니다.
    - JpaAuditing 을 통해 생성 및 수정 시간을 자동으로 기록하기 때문에 특정 기간별 조회 기능을 테스트 하기 위해 더미 데이터를 추가하였습니다.
    - 특정 기간 조회에서 기존 start 이상 end 이하로 조회하였던 것을 start 이상 end 미만으로 로직을 수정하였습니다.

---
+++ src/test/java/woohaengsi/qnadiary/RepositoryIntegrationTest.java
@@ -0,0 +1,13 @@
+package woohaengsi.qnadiary;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public abstract class RepositoryIntegrationTest {
+
+}
